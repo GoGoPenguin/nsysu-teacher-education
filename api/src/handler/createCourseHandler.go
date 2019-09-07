dfecfc2b9510cbb0d750329129b5fa45f5a27059
Paul <alphagoo@g-mail.nsysu.edu.tw>
2019-09-07 11:45:30 +0800

    [api] add create course api

--- api/src/handler/createCourseHandler.go
+++ api/src/handler/createCourseHandler.go
@@ -1,28 +1,46 @@
 package handler
 
 import (
+	"time"
+
 	"github.com/asaskevich/govalidator"
 	"github.com/kataras/iris"
 	"github.com/nsysu/teacher-education/src/error"
 	"github.com/nsysu/teacher-education/src/service"
+	t "github.com/nsysu/teacher-education/src/utils/time"
 )
 
 // CreateCourseHandler user login
 func CreateCourseHandler(ctx iris.Context) {
 	type rule struct {
-		Topic       string `valid:"required"`
-		Information string `valid:"required"`
-		Type        string `valid:"required, in(A|B|C)"`
-		Start       string `valid:"required"`
-		End         string `valid:"required"`
+		Topic       string    `valid:"required"`
+		Information string    `valid:"required"`
+		Type        string    `valid:"required, in(A|B|C)"`
+		Start       time.Time `valid:"required"`
+		End         time.Time `valid:"required"`
+	}
+
+	startTime, err := time.Parse(t.DateTime, ctx.FormValue("Start"))
+	if err != nil {
+		failed(ctx, error.ValidateError("Start: "+ctx.FormValue("Start")+" does not validate as timestamp"))
+		return
+	}
+	endTime, err := time.Parse(t.DateTime, ctx.FormValue("End"))
+	if err != nil {
+		failed(ctx, error.ValidateError("End: "+ctx.FormValue("Start")+" does not validate as timestamp"))
+		return
+	}
+	if !startTime.Before(endTime) {
+		failed(ctx, error.ValidateError("Start: "+ctx.FormValue("Start")+" does not after "+ctx.FormValue("End")))
+		return
 	}
 
 	params := &rule{
 		Topic:       ctx.FormValue("Topic"),
 		Information: ctx.FormValue("Information"),
 		Type:        ctx.FormValue("Type"),
-		Start:       ctx.FormValue("Start"),
-		End:         ctx.FormValue("End"),
+		Start:       startTime,
+		End:         endTime,
 	}
 
 	if _, err := govalidator.ValidateStruct(params); err != nil {
--- api/src/service/courseService.go
+++ api/src/service/courseService.go
@@ -1,8 +1,32 @@
 package service
 
-import "github.com/nsysu/teacher-education/src/error"
+import (
+	"time"
+
+	"github.com/nsysu/teacher-education/src/error"
+	"github.com/nsysu/teacher-education/src/persistence/gorm"
+	"github.com/nsysu/teacher-education/src/utils/logger"
+)
 
 // CreateCourse create a course
-func CreateCourse(topic, information, courseType, start, end string) (result interface{}, err *error.Error) {
-	return
+func CreateCourse(topic, information, courseType string, start, end time.Time) (result interface{}, e *error.Error) {
+	tx := gorm.DB()
+
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Error(r)
+			e = error.UnexpectedError()
+		}
+	}()
+
+	course := &gorm.Course{
+		Topic:       topic,
+		Information: information,
+		Type:        courseType,
+		Start:       start,
+		End:         end,
+	}
+	gorm.CourseDao.New(tx, course)
+
+	return "success", nil
 }
