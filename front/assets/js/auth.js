951ff2d183ed2e5f13611b6ae02e75c68fef1525
Paul <alphagoo@g-mail.nsysu.edu.tw>
2019-09-12 00:19:23 +0800

    [front] 1. translate to chinese 2. implement logout

--- front/assets/js/auth.js
+++ front/assets/js/auth.js
@@ -1,12 +1,59 @@
 $(document).ready(function () {
     let token = $.cookie('token')
+    let refreshToken = $.cookie('refresh-token')
 
-    if (token == undefined && window.location.pathname != '/login.html') {
-        location.href = '/login.html'
-    } else if (token != undefined && window.location.pathname == '/login.html') {
+    if (token == undefined) {
+        if (refreshToken != undefined) {
+            renewToken()
+            token = $.cookie('token')
+        } else if (window.location.pathname != '/login.html') {
+            location.href = '/login.html'
+        }
+    } else if (window.location.pathname == '/login.html') {
         location.href = '/'
     }
 })
+
+function renewToken() {
+    let account = $.cookie('account')
+    let refreshToken = $.cookie('refresh-token')
+
+    $.ajax({
+        url: config.server + '/v1/renew-token',
+        type: 'POST',
+        async: false,
+        cache: false,
+        data: {
+            Account: account,
+            RefreshToken: refreshToken,
+        },
+        error: function (xhr) {
+            let cookies = $.cookie()
+            for (var cookie in cookies) {
+                $.removeCookie(cookie)
+            }
+
+            location.href = '/login.html'
+        },
+        success: function (response) {
+            if (response.code != 0) {
+                let cookies = $.cookie();
+                for (var cookie in cookies) {
+                    $.removeCookie(cookie)
+                }
+
+                location.href = '/login.html'
+            } else {
+                let date = new Date()
+                date.setTime(date.getTime() + (response.data.Expire * 1000));
+
+                $.cookie('token', response.data.Token, {
+                    expires: date,
+                });
+            }
+        }
+    });
+}
 
 $('button#login').click(function () {
     let account = $('input.input100.account').val()
@@ -33,6 +80,7 @@ $('button#login').click(function () {
                 $.cookie('token', response.data.Token, {
                     expires: date,
                 });
+                $.cookie('account', account);
                 $.cookie('refresh-token', response.data.RefreshToken)
 
                 location.href = '/'
@@ -41,3 +89,36 @@ $('button#login').click(function () {
     });
 })
 
+$('#logoutModal button.btn.btn-primary').click(function () {
+    $.ajax({
+        url: config.server + '/v1/logout',
+        type: 'POST',
+        error: function (xhr) {
+            console.error(xhr);
+        },
+        beforeSend: function (xhr) {
+            let token = $.cookie('token')
+            if (token == undefined) {
+                renewToken()
+                token = $.cookie('token')
+            }
+
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        },
+        success: function (response) {
+            if (response.code != 0) {
+                console.error(response.message)
+            } else {
+                let date = new Date()
+                date.setTime(date.getTime() + (response.data.Expire * 1000));
+
+                let cookies = $.cookie()
+                for (var cookie in cookies) {
+                    $.removeCookie(cookie)
+                }
+
+                location.href = '/login.html'
+            }
+        }
+    });
+})
