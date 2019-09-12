e5a08225c63b0a0463ebe6f1176f4447e6911707
Paul <paul@emb.com>
2019-09-12 17:26:14 +0800

    [api] create table student_course

---
+++ api/db/migrations/20190912130459-create_table_student_course.sql
@@ -0,0 +1,23 @@
+
+-- +migrate Up
+CREATE TABLE `student_course` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `student_id` INT NOT NULL COMMENT '學生ID',
+    `course_id` INT NOT NULL COMMENT '講座ID',
+    `status` ENUM('pass', 'failed', '') NOT NULL DEFAULT '' COMMENT '狀態',
+    `review` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '心得',
+    `comment` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '備註',
+    `created_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建日期',
+    `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日期',
+    `deleted_at` DATETIME COMMENT '刪除日期',
+    INDEX (`status`),
+    INDEX (`deleted_at`),
+    INDEX (`student_id`),
+    INDEX (`course_id`),
+    FOREIGN KEY (`student_id`) REFERENCES `student`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`course_id`) REFERENCES `course`(`id`) ON DELETE CASCADE,
+    PRIMARY KEY(`id`)
+) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='學生研習';
+-- +migrate Down
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `student_course`;
