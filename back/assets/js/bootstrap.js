5f2e0fb3518c6974a9e9151ddbee9b6a8f775414
Paul <paul@emb.com>
2019-09-19 10:00:43 +0800

    [back] update bootstrap js

--- back/assets/js/bootstrap.js
+++ back/assets/js/bootstrap.js
@@ -11,3 +11,44 @@ if (token == undefined) {
 } else if (window.location.pathname == '/login.html') {
     location.href = '/'
 }
+
+function renewToken() {
+    let account = $.cookie('account')
+    let refreshToken = $.cookie('refresh-token')
+
+    $.ajax({
+        url: config.server + '/v1/renew-token',
+        type: 'POST',
+        async: false,
+        cache: false,
+        data: {
+            Account: account,
+            RefreshToken: refreshToken,
+        },
+        error: function (xhr) {
+            let cookies = $.cookie()
+            for (var cookie in cookies) {
+                $.removeCookie(cookie)
+            }
+
+            location.href = '/login.html'
+        },
+        success: function (response) {
+            if (response.code != 0) {
+                let cookies = $.cookie();
+                for (var cookie in cookies) {
+                    $.removeCookie(cookie)
+                }
+
+                location.href = '/login.html'
+            } else {
+                let date = new Date()
+                date.setTime(date.getTime() + (response.data.Expire * 1000));
+
+                $.cookie('token', response.data.Token, {
+                    expires: date,
+                });
+            }
+        }
+    });
+}
