60ae84f28ec51d15f1974b8c7d9ad0d72a272029
Paul <paul@emb.com>
2019-09-23 09:08:28 +0800

    [back] add student-course table

--- back/assets/js/course.js
+++ back/assets/js/course.js
@@ -1,3 +1,9 @@
+const STATUS = {
+    '': '未審核',
+    'pass': '通過',
+    'failed': '未通過',
+}
+
 $(document).ready(function () {
     $('table#course').DataTable({
         processing: true,
@@ -52,6 +58,70 @@ $(document).ready(function () {
         ],
         columnDefs: [
             { className: "info", targets: [2] },
+        ],
+        language: {
+            url: '/assets/languages/chinese.json'
+        },
+    });
+
+
+    $('table#student-course').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: false,
+        searching: false,
+        ajax: {
+            url: config.server + '/v1/course/sign-up',
+            type: 'GET',
+            dataSrc: function (d) {
+                d.list.forEach(function (element, index, array) {
+                    let startDate = array[index].Course.Start.substring(0, 10)
+                    let startTime = array[index].Course.Start.substring(11, 19)
+                    let endDate = array[index].Course.End.substring(0, 10)
+                    let endTime = array[index].Course.End.substring(11, 19)
+
+                    if (startDate == endDate) {
+                        array[index].Time = startDate + ' ' + startTime + ' ~ ' + endTime
+                    } else {
+                        array[index].Time = startDate + ' ' + startTime + ' ~ ' + endDate + ' ' + endTime
+                    }
+
+                    array[index].Status = STATUS[array[index].Status]
+                })
+                return d.list
+            },
+            beforeSend: function (xhr) {
+                let token = $.cookie('token')
+                if (token == undefined) {
+                    renewToken()
+                    token = $.cookie('token')
+                }
+
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            },
+            error: function (xhr, error, thrown) {
+                if (xhr.status == 401) {
+                    let cookies = $.cookie()
+                    for (var cookie in cookies) {
+                        $.removeCookie(cookie)
+                    }
+
+                    location.href = '/login.html'
+                } else {
+                    alert(xhr.responseText)
+                }
+            }
+        },
+        columns: [
+            { data: "Status" },
+            { data: "Student.Account" },
+            { data: "Student.Number" },
+            { data: "Student.Major" },
+            { data: "Student.Name" },
+            { data: "Meal" },
+            { data: "Course.Topic" },
+            { data: "Course.Type" },
+            { data: "Time" },
         ],
         language: {
             url: '/assets/languages/chinese.json'
