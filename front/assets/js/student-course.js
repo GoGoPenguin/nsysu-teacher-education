2595515270e1b92230b83acb6d68cd9087935511
Paul <paul@emb.com>
2019-09-24 09:01:36 +0800

    [front] implement update student-course review

--- front/assets/js/student-course.js
+++ front/assets/js/student-course.js
@@ -49,11 +49,61 @@ $(document).ready(function () {
                             <td>'+ element.Course.Type + '</td>\
                             <td>'+ STATUS[element.Status] + '</td>\
                             <td>'+ element.Comment + '</td>\
-                            <td><button class="btn btn-primary">心得上傳</button></td>\
+                            <td>'+ element.Review + '</td>\
+                            <td class="edit"><button class="btn btn-primary" id="' + element.ID + '">編輯</button></td>\
                         </tr>\
                     ')
                 })
             }
         }
     });
+})
+
+$('#student-course').on('click', '.edit', function () {
+    let id = $(this).children().get(0).id
+    let review = $(this).prev().html()
+
+    $('#updateReviewModal textarea').val(review)
+    $('#updateReviewModal input').val(id)
+    $('#updateReviewModal').modal('show')
+})
+
+$('#updateReviewModal form').on('submit', function (e) {
+    e.preventDefault()
+
+    let studentCourseID = $('#updateReviewModal input').val()
+    let review = $('#updateReviewModal textarea').val()
+
+    $.ajax({
+        url: config.server + '/v1/course/review',
+        type: 'PATCH',
+        error: function (xhr) {
+            console.error(xhr);
+        },
+        data: {
+            'StudentCourseID': studentCourseID,
+            'Review': review,
+        },
+        beforeSend: function (xhr) {
+            let token = $.cookie('token')
+            if (token == undefined) {
+                renewToken()
+                token = $.cookie('token')
+            }
+
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        },
+        success: function (response) {
+            $('#updateReviewModal').modal('hide')
+            $('button#' + studentCourseID).parent().prev().html(review)
+
+            $('#alert').addClass('alert alert-success')
+            $('#alert').html('成功<button type="button" class="close"><span aria-hidden="true">&times;</span></button>')
+            $('#alert').show('fast')
+        }
+    })
+})
+
+$('#alert').on('click', '.close', function () {
+    $('#alert').hide('slow')
 })
