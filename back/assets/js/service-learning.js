48408508cbd29ccd4a9aec7e4292b5ecfa688ca9
Paul <paul@emb.com>
2019-10-08 17:07:17 +0800

    [back] add studnt-service-learning table

--- back/assets/js/service-learning.js
+++ back/assets/js/service-learning.js
@@ -3,6 +3,11 @@ const TYPE = {
     'internship': '實習服務',
     'volunteer': '志工服務',
 }
+const STATUS = {
+    '': '未審核',
+    'pass': '通過',
+    'failed': '未通過',
+}
 
 const serviceLearningTable = $('table#service-learning').DataTable({
     processing: true,
@@ -53,6 +58,69 @@ const serviceLearningTable = $('table#service-learning').DataTable({
         { data: "Date" },
         { data: "Seesion" },
         { data: "Hours" },
+    ],
+    language: {
+        url: '/assets/languages/chinese.json'
+    },
+});
+
+const studentServiceLearningTable = $('table#student-service-learning').DataTable({
+    processing: true,
+    serverSide: true,
+    ordering: false,
+    searching: false,
+    ajax: {
+        url: config.server + '/v1/service-learning/sign-up',
+        type: 'GET',
+        dataSrc: (d) => {
+            d.list.forEach((element, index, array) => {
+                array[index].ServiceLearning.Type = TYPE[element.ServiceLearning.Type];
+                array[index].Status = STATUS[array[index].Status]
+                array[index].Date = element.ServiceLearning.Start.substring(0, 10) + ' ~ ' + element.ServiceLearning.End.substring(0, 10)
+                array[index].Button = '<button class="btn btn-primary" onclick="check(' + index + ')">審核</button>'
+            })
+            return d.list
+        },
+        beforeSend: (xhr) => {
+            let token = $.cookie('token')
+            if (token == undefined) {
+                renewToken()
+                token = $.cookie('token')
+            }
+
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        },
+        error: (xhr, error, thrown) => {
+            if (xhr.status == 401) {
+                let cookies = $.cookie()
+                for (var cookie in cookies) {
+                    $.removeCookie(cookie)
+                }
+
+                location.href = '/login.html'
+            } else {
+                swal({
+                    title: '',
+                    text: xhr.responseText,
+                    icon: "error",
+                    timer: 1000,
+                    buttons: false,
+                })
+            }
+        }
+    },
+    columns: [
+        { data: "Status" },
+        { data: "Student.Account" },
+        { data: "Student.Number" },
+        { data: "Student.Major" },
+        { data: "Student.Name" },
+        { data: "ServiceLearning.Type" },
+        { data: "ServiceLearning.Content" },
+        { data: "Date" },
+        { data: "ServiceLearning.Session" },
+        { data: "ServiceLearning.Hours" },
+        { data: "Button" },
     ],
     language: {
         url: '/assets/languages/chinese.json'
