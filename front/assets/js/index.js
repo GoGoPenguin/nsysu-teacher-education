46526d0b89f2744560470d89ed5d0a75b6a435cf
Paul <alphagoo@g-mail.nsysu.edu.tw>
2019-11-12 21:03:04 +0800

    [front] update leture section

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -4,6 +4,12 @@ const TYPE = {
     'volunteer': '志工服務',
 }
 
+$(document).ready(() => {
+    getCourses()
+    getServiceLearning()
+    getLetures()
+})
+
 const getCourses = () => {
     $.ajax({
         url: `${config.server}/v1/course`,
@@ -90,11 +96,39 @@ const getServiceLearning = () => {
     });
 }
 
-
-$(document).ready(() => {
-    getCourses()
-    getServiceLearning()
-})
+const getLetures = () => {
+    $.ajax({
+        url: `${config.server}/v1/leture`,
+        type: 'GET',
+        beforeSend: (xhr) => {
+            setHeader(xhr)
+        },
+        error: (xhr) => {
+            errorHandle(xhr, "錯誤")
+        },
+        success: (response) => {
+            if (response.list.length == 0) {
+                $('#leture tbody').append(`
+                    <tr>
+                        <td scope="row" colspan="7" style="text-align: center">尚無資料</td>
+                    </tr>
+                `)
+            } else {
+                response.list.forEach((element, index) => {
+                    $('#leture tbody').append(`
+                    <tr>
+                        <th scope="row">${index}</th>
+                        <td>${element.Name}</td>
+                        <td>${element.MinCredit}</td>
+                        <td>${element.Comment}</td>
+                        <td><button class="btn btn-secondary mr-3" onclick="detail(${element.ID})">查看</button><button class="btn btn-primary">報名</button></td>
+                    </tr>
+                `)
+                })
+            }
+        }
+    });
+}
 
 const getInformation = (id, filename) => {
     $.ajax({
@@ -197,6 +231,106 @@ const signUpServiceLearning = (id) => {
                 swal({
                     title: '',
                     text: '報名失敗',
+                    icon: "error",
+                    timer: 1500,
+                    buttons: false,
+                })
+            }
+        }
+    });
+}
+
+const detail = (id) => {
+    $.ajax({
+        url: `${config.server}/v1/leture/${id}`,
+        type: 'GET',
+        beforeSend: (xhr) => {
+            setHeader(xhr)
+        },
+        error: (xhr) => {
+            errorHandle(xhr, '失敗')
+        },
+        success: (response) => {
+            if (response.code === 0) {
+                let leture = response.data
+                let html = ''
+
+                for (let category of leture.Categories) {
+                    let content = ''
+                    let comment = ''
+                    let subjects = 0
+
+                    if (category.MinCredit > 1) {
+                        comment += `總共至少修習${category.MinCredit}學分<br>`
+                    }
+
+                    if (category.MinType > 1) {
+                        comment += `總共至少修習${category.MinType}類別<br>`
+                    }
+
+                    for (let type of category.Types) {
+                        let condition1 = category.Types.indexOf(type) === category.Types.length - 1
+                        let subjectGroups = 0
+
+                        if (type.MinCredit > 1) {
+                            comment += `${type.Name}至少修習${type.MinCredit}學分<br>`
+                        }
+
+                        for (let group of type.Groups) {
+                            let condition2 = type.Groups.indexOf(group) === type.Groups.length - 1
+
+                            subjects += group.Subjects.length
+                            subjectGroups += group.Subjects.length
+
+                            for (let subject of group.Subjects) {
+                                let condition3 = group.Subjects.indexOf(subject) === group.Subjects.length - 1
+                                let temp = '<tr>'
+
+                                if (condition1 && condition2 && condition3) {
+                                    temp += `<td colspan="2" rowspan="${subjects}" class="align-middle">${category.Name}</td>`
+                                }
+
+                                if (condition2 && condition3) {
+                                    temp += `<td colspan="2" rowspan="${subjectGroups}" class="align-middle">${type.Name}</td>`
+                                }
+
+                                temp += `<td colspan="1" class="align-middle">${subject.Compulsory ? "必修" : "選修"}</td>`
+
+                                if (group.MinCredit > 0) {
+                                    temp += `<td colspan="3" class="align-middle">${subject.Name}</td>`
+
+                                    if (condition3) {
+                                        temp += `<td colspan="1" rowspan="${group.Subjects.length}" class="align-middle vericaltext">至少${group.MinCredit}學分</td>`
+                                    }
+                                } else {
+                                    temp += `<td colspan="4" class="align-middle">${subject.Name}</td>`
+                                }
+
+                                temp += `<td colspan="1" class="align-middle">${subject.Credit}</td>`
+
+                                if (condition1 && condition2 && condition3) {
+                                    temp += `<td colspan="2" rowspan="${subjects}" class="align-middle">${comment}</td>`
+                                }
+
+                                temp += `</tr>`
+                                content = `${temp}${content}`
+                            }
+                        }
+                    }
+
+                    html += content
+                }
+
+                $('#detailModal .modal-title').html(leture.Name)
+                $('#detailModal #name').html(leture.Name)
+                $('#detailModal #min_credit').html(leture.MinCredit)
+                $('#detailModal #comment').html(leture.Comment)
+                $('#detailModal #categories').html(html)
+                $('#detailModal').modal('show')
+            } else {
+                swal({
+                    title: '',
+                    text: '失敗',
                     icon: "error",
                     timer: 1500,
                     buttons: false,
