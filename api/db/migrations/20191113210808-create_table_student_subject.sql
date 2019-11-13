407b404f352427a72ea15fed04133d76b380e231
Paul <alphagoo@g-mail.nsysu.edu.tw>
2019-11-13 22:47:24 +0800

    [api] add two db tables

---
+++ api/db/migrations/20191113210808-create_table_student_subject.sql
@@ -0,0 +1,20 @@
+
+-- +migrate Up
+CREATE TABLE `student_subject` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `student_leture_id` INT NOT NULL COMMENT '學生課程ID',
+    `subject_id` INT NOT NULL COMMENT '科目ID',
+    `pass` BOOLEAN NOT NULL COMMENT '通過',
+    `created_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建日期',
+    `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日期',
+    `deleted_at` DATETIME COMMENT '刪除日期',
+    INDEX (`pass`),
+    INDEX (`deleted_at`),
+    UNIQUE INDEX (`student_leture_id`, `subject_id`),
+    FOREIGN KEY (`student_leture_id`) REFERENCES `student_leture`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`subject_id`) REFERENCES `subject`(`id`) ON DELETE CASCADE,
+    PRIMARY KEY(`id`)
+) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='學生科目';
+-- +migrate Down
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `student_subject`;
