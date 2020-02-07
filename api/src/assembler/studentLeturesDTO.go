5bfed9388655225e4e0feaf8471662f7907e9429
Paul <paul@emb.com>
2020-02-07 11:56:29 +0800

    [back] fix dto bug

--- api/src/assembler/studentLeturesDTO.go
+++ api/src/assembler/studentLeturesDTO.go
@@ -9,28 +9,30 @@ func StudentLeturesDTO(studentLetures *[]gorm.StudentLeture) []map[string]interf
 	result := []map[string]interface{}{}
 
 	for _, studentLeture := range *studentLetures {
-		temp := map[string]interface{}{
-			"ID": studentLeture.ID,
+		if studentLeture.Leture.ID != 0 {
+			temp := map[string]interface{}{
+				"ID": studentLeture.ID,
 
-			"Leture": map[string]interface{}{
-				"ID":        studentLeture.Leture.ID,
-				"Name":      studentLeture.Leture.Name,
-				"MinCredit": studentLeture.Leture.MinCredit,
-				"Comment":   studentLeture.Leture.Comment,
-				"Status":    studentLeture.Leture.Status,
-			},
-			"Pass": studentLeture.Pass,
-		}
-		if studentLeture.Student.ID != 0 {
-			temp["Student"] = map[string]interface{}{
-				"ID":      studentLeture.Student.ID,
-				"Name":    studentLeture.Student.Name,
-				"Account": studentLeture.Student.Account,
-				"Major":   studentLeture.Student.Major,
-				"Number":  studentLeture.Student.Number,
+				"Leture": map[string]interface{}{
+					"ID":        studentLeture.Leture.ID,
+					"Name":      studentLeture.Leture.Name,
+					"MinCredit": studentLeture.Leture.MinCredit,
+					"Comment":   studentLeture.Leture.Comment,
+					"Status":    studentLeture.Leture.Status,
+				},
+				"Pass": studentLeture.Pass,
 			}
+			if studentLeture.Student.ID != 0 {
+				temp["Student"] = map[string]interface{}{
+					"ID":      studentLeture.Student.ID,
+					"Name":    studentLeture.Student.Name,
+					"Account": studentLeture.Student.Account,
+					"Major":   studentLeture.Student.Major,
+					"Number":  studentLeture.Student.Number,
+				}
+			}
+			result = append(result, temp)
 		}
-		result = append(result, temp)
 	}
 	return result
 }
--- api/src/assembler/studentServiceLearningsDTO.go
+++ api/src/assembler/studentServiceLearningsDTO.go
@@ -9,28 +9,30 @@ func StudentServiceLearningsDTO(studentServiceLearnings *[]gorm.StudentServiceLe
 	result := []map[string]interface{}{}
 
 	for _, studentServiceLearning := range *studentServiceLearnings {
-		result = append(result, map[string]interface{}{
-			"ID": studentServiceLearning.ID,
-			"Student": map[string]interface{}{
-				"Name":    studentServiceLearning.Student.Name,
-				"Account": studentServiceLearning.Student.Account,
-				"Major":   studentServiceLearning.Student.Major,
-				"Number":  studentServiceLearning.Student.Number,
-			},
-			"ServiceLearning": map[string]interface{}{
-				"ID":      studentServiceLearning.ServiceLearning.ID,
-				"Type":    studentServiceLearning.ServiceLearning.Type,
-				"Content": studentServiceLearning.ServiceLearning.Content,
-				"Session": studentServiceLearning.ServiceLearning.Session,
-				"Hours":   studentServiceLearning.ServiceLearning.Hours,
-				"Start":   studentServiceLearning.ServiceLearning.Start,
-				"End":     studentServiceLearning.ServiceLearning.End,
-			},
-			"Status":    studentServiceLearning.Status,
-			"Review":    studentServiceLearning.Review,
-			"Reference": studentServiceLearning.Reference,
-			"Comment":   studentServiceLearning.Comment,
-		})
+		if studentServiceLearning.ServiceLearning.ID != 0 {
+			result = append(result, map[string]interface{}{
+				"ID": studentServiceLearning.ID,
+				"Student": map[string]interface{}{
+					"Name":    studentServiceLearning.Student.Name,
+					"Account": studentServiceLearning.Student.Account,
+					"Major":   studentServiceLearning.Student.Major,
+					"Number":  studentServiceLearning.Student.Number,
+				},
+				"ServiceLearning": map[string]interface{}{
+					"ID":      studentServiceLearning.ServiceLearning.ID,
+					"Type":    studentServiceLearning.ServiceLearning.Type,
+					"Content": studentServiceLearning.ServiceLearning.Content,
+					"Session": studentServiceLearning.ServiceLearning.Session,
+					"Hours":   studentServiceLearning.ServiceLearning.Hours,
+					"Start":   studentServiceLearning.ServiceLearning.Start,
+					"End":     studentServiceLearning.ServiceLearning.End,
+				},
+				"Status":    studentServiceLearning.Status,
+				"Review":    studentServiceLearning.Review,
+				"Reference": studentServiceLearning.Reference,
+				"Comment":   studentServiceLearning.Comment,
+			})
+		}
 	}
 	return result
 }
